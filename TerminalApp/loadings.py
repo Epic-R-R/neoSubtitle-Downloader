c5efe7d0e233453adb1df7098aa2023ce7bd7095
Epic-R-R <epicrr001@gmail.com>
2021-04-22 16:33:22 +0430

    update code and run black sort on it

--- TerminalApp/loadings.py
+++ TerminalApp/loadings.py
@@ -1,10 +1,11 @@
 from halo import Halo
 from time import sleep
+import sys
 
 
 def loadingwelcome():
     spinner = Halo(stream=sys.stderr)
     spinner.start("Loading")
-    sleep(3)
+    sleep(2)
     spinner.stop()
     spinner.succeed("Loading Completed.")
