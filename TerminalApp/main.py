dba5bbe1b70311b59a932800e8a5c2697f42eceb
Epic-R-R <epicrr001@gmail.com>
2021-04-22 16:26:19 +0430

    create welcome page

---
+++ TerminalApp/main.py
@@ -0,0 +1,69 @@
+from PyInquirer import prompt
+from styles import style
+import os
+from pyfiglet import Figlet
+import sys
+from colorama import init, Fore
+init(autoreset=True)
+
+def welcome_page():
+    os.system("clear")
+    ssd = Figlet(font="slant")
+    print(f"{Fore.LIGHTMAGENTA_EX}{ssd.renderText('neoSubtitle DW')}")
+    print(f"{Fore.BLUE}Welcome To neoSubtitle Downloader")
+    info = [
+        {
+            "type": "list",
+            "name": "info",
+            "message": "Choose:",
+            "choices": [
+                "Sign in",
+                "Sign up",
+                "Explorer",
+                "Why use neoSubtitle Downloader ?",
+                "Donate",
+                "Exit",
+            ],
+        }
+    ]
+    ans = prompt(info, style=style)
+    if ans["info"] == "Sign in":
+        # if signIn() == "Username or password is wrong":
+        #     welcome_page()
+        # else:
+        #     main()
+        pass
+    elif ans["info"] == "Sign up":
+        # if signUp() == "Registration was successful":
+        #     if signIn() == "Username or password is wrong":
+        #         welcome_page()
+        #     else:
+        #         main()
+        # elif signUp() == "Username early exists" or signUp() == "SQL Error":
+        #     welcome_page()
+        pass
+    elif ans["info"] == "Explorer":
+        pass
+    elif ans["info"] == "Why use neoSubtitle Downloader ?":
+        pass
+    elif ans["info"] == "Donate":
+        pass
+    elif ans["info"] == "Exit":
+        confirm = [
+            {
+                "type": "confirm",
+                "message": "Do you want to exit?",
+                "name": "exit",
+                "default": False,
+            },
+        ]
+        ans = prompt(confirm, style=style)
+        if ans["exit"]:
+            sys.exit(0)
+        else:
+            welcome_page()
+
+
+if __name__ == "__main__":
+    init(autoreset=True) # Colorama autoreset terminal color True
+    welcome_page()
